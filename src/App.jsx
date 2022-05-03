fea0503ec75d21cc1d2aadc0e891856d44e46efd
Javi <javierlopezblasco@gmail.com>
2022-05-03 21:37:06 +0200

    Install react-router-dom, create routes and install firebase

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<div>SHOP PAGE</div>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
